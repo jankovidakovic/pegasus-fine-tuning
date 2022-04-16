a66a14522ee8f50df93e859a099744da55f23f7e
jankovidakovic <janko.vidakovic@fer.hr>
2022-04-16 13:58:42 +0200

    Cleanup before running.

--- utils.py
+++ utils.py
@@ -1,22 +1,7 @@
-import torch
-from torch.utils.data import Dataset
 from transformers import PegasusForConditionalGeneration, Adafactor
 from transformers.optimization import AdafactorSchedule
 
-
-class PegasusDataset(Dataset):
-    def __init__(self, encodings, labels):
-        # TODO - check what this is
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels['input_ids'][idx])  # torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels['input_ids'])  # len(self.labels)
+from pegasus_dataset import PegasusDataset
 
 
 def prepare_model(*, device: str = "cuda"):
