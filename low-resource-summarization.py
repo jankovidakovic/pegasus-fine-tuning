2a01b6e8423bf71587d76f4fccccccf32e8dae9a
jankovidakovic <janko.vidakovic@fer.hr>
2022-04-23 16:19:23 +0200

    Finished script.

--- low-resource-summarization.py
+++ low-resource-summarization.py
@@ -26,28 +26,6 @@ class PegasusDataset(Dataset):
         return len(self.labels['input_ids'])  # len(self.labels)
 
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    # Replace -100 in the labels as we can't decode them.
-    # labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # Rouge expects a newline after each sentence
-    decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
-    decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
-
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels,
-                            use_stemmer=True)  # not sure if using stemmer is correct here
-
-    # Extract a few results
-    result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
-
-    # combine rouge scores for model comparison
-    result["rougecomb"] = result["rouge1"] + 2 * result["rouge2"] + result["rougeLsum"]
-    return result
-
-
 """
         output_dir=output_dir,  # output directory
         per_device_train_batch_size=1,  # batch size per device during training, can increase if memory allows
@@ -99,9 +77,10 @@ DEFAULT_TRAINING_ARGUMENTS = {
 
     "per_device_train_batch_size": 1,  # batch size per device during training, can increase if memory allows
     "gradient_accumulation_steps": 128,
+    # "gradient_checkpointing": True,
 
     "per_device_eval_batch_size": 1,  # batch size for evaluation, can increase if memory allows
-    "eval_accumulation_steps": 128,
+    # "eval_accumulation_steps": 128,
 
     "evaluation_strategy": IntervalStrategy.STEPS,  # evaluation strategy to adopt during training
     "eval_steps": 100,
@@ -118,16 +97,17 @@ DEFAULT_TRAINING_ARGUMENTS = {
 
     "adafactor": True,
     "optim": OptimizerNames.ADAFACTOR,
-    "lr_scheduler_type": SchedulerType.CONSTANT
+    "lr_scheduler_type": SchedulerType.CONSTANT,
+    "label_smoothing_factor": 0.1
 }
 
 
-if __name__ == "__main__":
+def main():
     parser = create_parser()
     args = parser.parse_args()
-    config = vars(args)
+    cli_config = vars(args)
 
-    wandb.init(project=config["wandb_project"], entity="jankovidakovic")
+    wandb.init(project=cli_config["wandb_project"], entity="jankovidakovic")
 
     dataset = load_dataset("cnn_dailymail", "3.0.0")
     metric = load_metric("rouge")
@@ -135,13 +115,12 @@ if __name__ == "__main__":
     train_articles, train_summaries = dataset['train']['article'], dataset['train']['highlights']
     eval_articles, eval_summaries = dataset["validation"]["article"], dataset["validation"]["highlights"]
 
-    train_cutoff, eval_cutoff = config["n_train_examples"], config["n_eval_examples"]
+    train_cutoff, eval_cutoff = cli_config["n_train_examples"], cli_config["n_eval_examples"]
     train_articles, train_summaries = train_articles[:train_cutoff], train_summaries[:train_cutoff]
     eval_articles, eval_summaries = eval_articles[:eval_cutoff], eval_summaries[:eval_cutoff]
 
     model_name = 'google/pegasus-large'
     torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    output_dir = "test-results"
 
     # prepare training data
     tokenizer = PegasusTokenizerFast.from_pretrained(model_name)
@@ -152,6 +131,26 @@ if __name__ == "__main__":
         dataset_tokenized = PegasusDataset(article_encodings, summary_encodings)
         return dataset_tokenized
 
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        # Replace -100 in the labels as we can't decode them.
+        # labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        # Rouge expects a newline after each sentence
+        decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+        decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels,
+                                use_stemmer=True)  # not sure if using stemmer is correct here
+
+        # Extract a few results
+        result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+
+        # combine rouge scores for model comparison
+        result["rougecomb"] = result["rouge1"] + 2 * result["rouge2"] + result["rougeLsum"]
+        return result
 
     train_dataset = tokenize_data(train_articles, train_summaries)
     eval_dataset = tokenize_data(eval_articles, eval_summaries)
@@ -169,29 +168,39 @@ if __name__ == "__main__":
 
     scheduler = AdafactorSchedule(optimizer, initial_lr=5e-4)
 
-    training_args = Seq2SeqTrainingArguments(
-        output_dir=output_dir,  # output directory
-        max_steps=2000,
-        per_device_train_batch_size=1,  # batch size per device during training, can increase if memory allows
-        per_device_eval_batch_size=1,  # batch size for evaluation, can increase if memory allows
-        gradient_accumulation_steps=128,
-        eval_accumulation_steps=128,
-        # save_steps=1000,  # number of updates steps before checkpoint saves
-        save_steps=1,
-        save_total_limit=1,  # limit the total amount of checkpoints and deletes the older checkpoint
-        load_best_model_at_end=True,
-        metric_for_best_model="rougecomb",
-        evaluation_strategy=IntervalStrategy.STEPS,  # evaluation strategy to adopt during training
-        # eval_steps=10000,  # number of update steps before evaluation
-        eval_steps=1,
-        predict_with_generate=True,
-        logging_dir='./test-logs',  # directory for storing logs
-        logging_steps=1,
-        adafactor=True,
-        # learning_rate=5e-4,
-        optim=OptimizerNames.ADAFACTOR,
-        lr_scheduler_type=SchedulerType.CONSTANT
-    )
+    final_training_args = {k: cli_config.get(k, v) for k, v in DEFAULT_TRAINING_ARGUMENTS.items()}
+
+    device_count = torch.cuda.device_count()
+    final_training_args["gradient_accumulation_steps"] = \
+        cli_config["total_train_batch_size"] / cli_config["per_device_train_batch_size"] / device_count
+    final_training_args["eval_gradient_accumulation_steps"] = \
+        cli_config["total_train_batch_size"] / cli_config["per_device_train_batch_size"] / device_count
+
+    # training_args = Seq2SeqTrainingArguments(
+    # output_dir=output_dir,  # output directory
+    # max_steps=2000,
+    # per_device_train_batch_size=1,  # batch size per device during training, can increase if memory allows
+    # per_device_eval_batch_size=1,  # batch size for evaluation, can increase if memory allows
+    # gradient_accumulation_steps=128,
+    # eval_accumulation_steps=128,
+    # # save_steps=1000,  # number of updates steps before checkpoint saves
+    # save_steps=1,
+    # save_total_limit=1,  # limit the total amount of checkpoints and deletes the older checkpoint
+    # load_best_model_at_end=True,
+    # metric_for_best_model="rougecomb",
+    # evaluation_strategy=IntervalStrategy.STEPS,  # evaluation strategy to adopt during training
+    # # eval_steps=10000,  # number of update steps before evaluation
+    # eval_steps=1,
+    # predict_with_generate=True,
+    # logging_dir='./test-logs',  # directory for storing logs
+    # logging_steps=1,
+    # adafactor=True,
+    # # learning_rate=5e-4,
+    # optim=OptimizerNames.ADAFACTOR,
+    # lr_scheduler_type=SchedulerType.CONSTANT
+    # )
+
+    training_args = Seq2SeqTrainingArguments(**final_training_args)
 
     trainer = Seq2SeqTrainer(
         model=model,  # the instantiated 🤗 Transformers model to be trained
@@ -206,3 +215,7 @@ if __name__ == "__main__":
 
     # return trainer
     trainer.train()
+
+
+if __name__ == "__main__":
+    main()
